eb4c6e298f2d231e8ad0fb0628d912acdf35cc81
Fabio Ferreira-Soares <fferreir@fr-b-tb01635.local>
2019-02-17 20:39:16 +0100

    mongodb

--- backend/routes/user/user.routes.js
+++ backend/routes/user/user.routes.js
@@ -1,5 +1,6 @@
-const express       = require('express');
-const userRouter    = express.Router({ mergeParams: true });
+const express                   = require('express');
+const userRouter                = express.Router({ mergeParams: true });
+const { register, login }       = require('./user.controller');
 
 
 class UserRouterClass {
@@ -9,6 +10,25 @@ class UserRouterClass {
         userRouter.get('/', (req, res) => {
             res.json('HATEOAS for auth');
         });
+
+        // Register
+        userRouter.post('/register', (req, res) => {
+            // Use controller function
+            
+            register(req.body)
+            //.then( apiResponse => res.json(apiResponse) )
+            //.catch( apiResponse => res.json(apiResponse) )
+        });
+
+        // Login
+        userRouter.post('/login', (req, res) => {
+            // Use controller function
+            //console.log('REQ : ', req.body)
+            login(req.body)
+            //.then( apiResponse => res.json(apiResponse) )
+            //.catch( apiResponse => res.json(apiResponse) )
+        });
+
         
     };
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@
 */
                           require('dotenv').config();
 const express           = require('express');
+const bodyParser        = require('body-parser');
+
 const { mainRouter }    = require('./routes/main.router');
 
 /*
@@ -11,6 +13,7 @@ const { mainRouter }    = require('./routes/main.router');
 const port = process.env.PORT;
 const host = process.env.HOST;
 const server = express();
+const db = require('./services/db');
 
 
 
@@ -18,9 +21,18 @@ const server = express();
     Server
 */
 const init = () => {
+
+    // MongoDB
+    db.initClient()
     
+    //Body-parser
+    server.use(bodyParser.json({limit: '10mb'}));
+    server.use(bodyParser.urlencoded({ extended: true }));
+    
+    // Router
     server.use('/', mainRouter);
 
+    //Lunch
     server.listen(port, host, () => {
         console.log(`Server is running on ${host}:${port}`)
     });
